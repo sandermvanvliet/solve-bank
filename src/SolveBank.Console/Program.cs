a0ebbafd60d7d7cc1a06faec82eb2ec6e0acfb67
Sander van Vliet <s.vanvliet@coolblue.nl>
2018-07-24 20:56:16 +0200

    [CLI] Alternate way of running to allow for container overrides

--- src/SolveBank.Console/Program.cs
+++ src/SolveBank.Console/Program.cs
@@ -1,31 +1,58 @@
-﻿using Oakton;
+﻿using System;
+using Oakton;
 using SimpleInjector;
 using SolveBank.Adapters.InMemory;
 
 namespace SolveBank.Console
 {
-    class Program
+    public class Program
     {
+        private readonly CommandExecutor _executor;
+
         static void Main(string[] args)
+        {
+            var program = new Program();
+
+            program.Run(args);
+        }
+
+        public Program()
         {
             var container = BootstrapContainerAndAdapters();
 
-            var executor = CommandExecutor.For(_ =>
-                    _.RegisterCommands(typeof(Program).Assembly),
-                new SimpleInjectorCommandCreator(container));
-
-            executor.Execute(args);
+            _executor = CommandExecutor
+                .For(_ => _.RegisterCommands(typeof(Program).Assembly),
+                    new SimpleInjectorCommandCreator(container));
         }
 
-        private static Container BootstrapContainerAndAdapters()
+        private void Run(string[] args)
+        {
+            try
+            {
+                _executor.Execute(args);
+            }
+            catch (Exception ex)
+            {
+                System.Console.Error.WriteLine($"Oops! Something went very wrong, I'm sorry. Please show this message to your friendly neighbourhood developer: {ex.Message}");
+                Environment.Exit(1);
+            }
+        }
+
+        private Container BootstrapContainerAndAdapters()
         {
             var container = new Container();
 
             InMemoryAdapter.Register(container);
 
+            ApplyContainerOverrides();
+
             container.Verify();
 
             return container;
+        }
+
+        protected virtual void ApplyContainerOverrides()
+        {
         }
     }
 }
